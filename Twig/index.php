e0009de560cdccdf18908b16ccb4c6d70d6758ff
Cheeto <martin.bronnimann.pj@gmail.com>
2023-01-25 17:47:46 +0100

    Page Active Twig

--- Twig/index.php
+++ Twig/index.php
@@ -15,16 +15,18 @@
     $pages = array_diff(scandir("./templates/"), array('.', '..')); // Récupère les pages existantes
     $pages = array_diff($pages, array("base.twig")); // On enlève le base.twig car pas besoin pour les pages
     
+    $ajoutData = [
+        "pages" => array_keys($dataPages), // <-- Les pages existantes dans le data-pages pour les mettres dans la navbar automatiquement
+        "active" => $finalEndpoint //  <-- L'endpoint pour vérifier la page active
+    ];
+
     // On va vérifier si l'endpoint est dans la liste des pages
     if ($finalEndpoint != "" && in_array($finalEndpoint.".twig", $pages)) {
-        echo $twig->render($finalEndpoint.".twig", array_merge($dataPages[$finalEndpoint], [
-            "pages" => array_keys($dataPages), // <-- Les pages existantes dans le data-pages pour les mettres dans la navbar automatiquement
-            "active" => $finalEndpoint //  <-- L'endpoint pour vérifier la page active
-        ]));
+        echo $twig->render($finalEndpoint.".twig", array_merge($dataPages[$finalEndpoint], $ajoutData));
     } else {
         // Le "" n'est pas dans la liste des pages, cependant, on doit afficher le index quand l'url n'a pas d'endpoint
         if ($finalEndpoint == "") {
-            echo $twig->render("accueil.twig", $dataPages["accueil.twig"]);
+            echo $twig->render("accueil.twig", array_merge($dataPages["accueil"], $ajoutData));
         } else {
             // Erreur 404
             echo "404";
